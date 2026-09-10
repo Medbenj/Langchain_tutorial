61819fb599bbb34158900808bca438daf175934c
Medbenj <ahmadbenjrinija12@gmail.com>
2026-09-10 12:14:05 +0100

    Remove LANGSMITH_API_KEY retrieval and print statement from Deep-search-agent.py

--- Deep-search-agent.py
+++ Deep-search-agent.py
@@ -1,9 +1,7 @@
 import os
 from dotenv import load_dotenv
 load_dotenv()
-LANGSMITH_API_KEY = os.getenv("LANGSMITH_API_KEY")
 
-print(f"Your API Key is: {LANGSMITH_API_KEY}")
 
 import os
 from typing import Annotated, Literal
